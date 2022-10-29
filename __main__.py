0cae8995bf7de0fcfc23cacbac52ad4b36e4e8d1
matthirdler <mattettler@gmail.com>
2022-10-29 14:32:38 -0600

    feat(__main__): add attribute generation

--- __main__.py
+++ __main__.py
@@ -2,14 +2,17 @@
 import logging
 import sys
 from app import category_generator as cg
+from app import attribute_generator as ag
 
 categories = ["Wildlife", "Automobiles", "Health"]
+attributes = ["Artistic", "Functional", "Excessive"]
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     print("Running project-generator...")
     category = cg.get_random_category(categories)
     print(f"Selected category \"{category}\" for your project!")
-
+    attribute = ag.generate_attribute(attributes)
+    print(f"Selected attribute \"{attribute}\" for your project!")
     logging.info("Program exiting normally.")
     sys.exit(0)
