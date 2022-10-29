be3c0dd08f49b587494f30eb1c1200c9ae96a3ed
matthirdler <mattettler@gmail.com>
2022-10-29 15:02:16 -0600

    refactor: update logging

--- app/attribute_generator.py
+++ app/attribute_generator.py
@@ -1,9 +1,11 @@
 """Module responsible for generating attributes"""
+import logging
 import random
 from app import errors
 
 def generate_attribute(attributes):
     """Chooses an attribute from the input list and returns it"""
+    logging.info("Generating attribute...")
     if not isinstance(attributes, list) or len(attributes) == 0:
         raise errors.NoAttributesFoundError()
     return random.choice(attributes)
--- app/category_generator.py
+++ app/category_generator.py
@@ -5,7 +5,7 @@ from app import errors
 
 def generate_category(unordered_categories):
     """Get a single random category selected from the input list"""
-    logging.info("Getting random category...")
+    logging.info("Generating category...")
     if not isinstance(unordered_categories, list) or len(unordered_categories) == 0:
         raise errors.NoCategoriesFoundError()
     return random.choice(unordered_categories)
