2a39438b72c20eb7d51f3b11142b81bf6c0c2630
matthirdler <mattettler@gmail.com>
2022-10-29 14:46:04 -0600

    feat(__main__): add configurable attributes

--- __main__.py
+++ __main__.py
@@ -3,16 +3,16 @@ import logging
 import sys
 from app import category_generator as cg
 from app import attribute_generator as ag
+import attribute_config
 
 categories = ["Wildlife", "Automobiles", "Health"]
-attributes = ["Artistic", "Functional", "Excessive"]
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     print("Running project-generator...")
     category = cg.generate_category(categories)
     print(f"Selected category \"{category}\" for your project!")
-    attribute = ag.generate_attribute(attributes)
+    attribute = ag.generate_attribute(attribute_config.attributes)
     print(f"Selected attribute \"{attribute}\" for your project!")
     logging.info("Program exiting normally.")
     sys.exit(0)
---
+++ attribute_config.py
@@ -0,0 +1,21 @@
+"""Configurable list of attributes"""
+
+attributes = [
+    "Local",
+    "Available",
+    "Evergreen",
+    "Portable",
+    "Modest",
+    "Grand",
+    "Organized",
+    "Cooperative",
+    "Personable",
+    "Cold",
+    "Warm",
+    "Extensible",
+    "Rigid",
+    "Greedy",
+    "Forgiving",
+    "Efficient",
+    "Methodical"
+]
