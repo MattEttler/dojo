dc68775e7e188e03231cf80e1f6171ab4418b6c9
matthirdler <mattettler@gmail.com>
2022-10-29 14:54:39 -0600

    feat(__main__): add configurable categories

--- __main__.py
+++ __main__.py
@@ -3,14 +3,13 @@ import logging
 import sys
 from app import category_generator as cg
 from app import attribute_generator as ag
+import category_config
 import attribute_config
-
-categories = ["Wildlife", "Automobiles", "Health"]
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     print("Running project-generator...")
-    category = cg.generate_category(categories)
+    category = cg.generate_category(category_config.categories)
     print(f"Selected category \"{category}\" for your project!")
     attribute = ag.generate_attribute(attribute_config.attributes)
     print(f"Selected attribute \"{attribute}\" for your project!")
---
+++ category_config.py
@@ -0,0 +1,23 @@
+"""Configurable list of categories mostly sourced from https://wheeloffortunehistory.fandom.com/wiki/List_of_categories"""
+
+categories = [
+    "Around the House",
+    "Before & After",
+    "Best Seller(s)",
+    "Character(s)",
+    "Classic TV",
+    "College Life",
+    "Event(s)",
+    "Family",
+    "Fun & Games",
+    "Food & Drink",
+    "Headline",
+    "Husband & Wife",
+    "In the Kitchen",
+    "Landmark(s)",
+    "In the Kitchen",
+    "Living Things",
+    "Occupation(s)",
+    "Music",
+    "Botany"
+]
